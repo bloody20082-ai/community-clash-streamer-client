caffd8b253c84d3eb6615fb81749fd5cf5de3456
bloody20082-ai <bloody20082@gmail.com>
2026-09-24 20:45:42 +0200

    Use V3.4 audio capture interface

--- src/loopback_audio.h
+++ src/loopback_audio.h
@@ -5,16 +5,22 @@
 #include <thread>
 #include <vector>
 
-class LoopbackAudio {
+class LoopbackAudioCapture {
 public:
   using AudioCallback =
-      std::function<void(std::vector<std::int16_t>&&, unsigned, unsigned)>;
+      std::function<void(int sample_rate, int channels, std::vector<std::int16_t>&&)>;
 
-  ~LoopbackAudio();
+  ~LoopbackAudioCapture();
+
   void start(AudioCallback cb);
   void stop();
+
+  int sample_rate() const noexcept { return sample_rate_.load(); }
+  std::uint64_t blocks() const noexcept { return blocks_.load(); }
 
 private:
   std::atomic<bool> running_{false};
   std::thread thread_;
+  std::atomic<int> sample_rate_{48000};
+  std::atomic<std::uint64_t> blocks_{0};
 };
